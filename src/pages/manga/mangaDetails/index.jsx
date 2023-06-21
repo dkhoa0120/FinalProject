9efba7da42cdf6598a155d7fafd242a15886c322
huy26062002 <nonemu211@gmail.com>
2023-06-21 11:32:59 +0700

    Improve manga-details

--- src/pages/manga/mangaDetails/index.jsx
+++ src/pages/manga/mangaDetails/index.jsx
@@ -60,54 +60,65 @@ function MangaDetail() {
                   {manga ? (
                     <>
                       <Row>
-                  <h1 className="txt-rotate">{manga.originalTitle}</h1>
-                  <span>TestAuthorName</span>
-                  <p>{manga.description}</p>
-                </Row>
-                <Row>
-                  <Col>
-                  <div>
-                          {manga.categories.map((c) => (
-                            <Button key={c.id} variant="outline-dark"style={{margin:"0 10px 0 0"}}>{c.name}</Button>
-                          ))}
-                      </div>
-                  </Col>
-                  <Col>
-                    <span>Publication: {manga.publishYear}</span>
-                  </Col>
-                </Row>
-                &nbsp;
-                <p>Alternative Titles: {manga.alternativeTitles}</p>
-                <Row>
-                  <Col>
-                    <Dropdown>
-                      <Dropdown.Toggle variant="outline" id="dropdown-basic">
-                        <span>
-                          <i className="fa-regular fa-star"></i>
-                        </span>
-                      </Dropdown.Toggle>
-                      <Dropdown.Menu>
-                        <Dropdown.Item>(5) Masterpice</Dropdown.Item>
-                        <Dropdown.Item>(4) Good</Dropdown.Item>
-                        <Dropdown.Item>(3) Fine</Dropdown.Item>
-                        <Dropdown.Item>(2) Bad</Dropdown.Item>
-                        <Dropdown.Item>(1) Horrible</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Col>
-                  <Col>
-                    &nbsp;&nbsp;
-                    <span>
-                      <i className="fa-regular fa-heart"></i>&nbsp;100
-                    </span>
-                  </Col>
-                </Row>
+                        <h1 className="txt-rotate">{manga.originalTitle}</h1>
+                        <p>Alternative Titles: {manga.alternativeTitles}</p>
+                        <p>TestAuthorName</p>
+                        <p>{manga.description}</p>
+                      </Row>
+                      <Row>
+                        <Col sm="9">
+                          <div>
+                            {manga.categories.map((c) => (
+                              <Button
+                                key={c.id}
+                                variant="outline-dark"
+                                style={{ margin: "0 10px 0 0" , border: "none"}}
+                              >
+                                {c.name}
+                              </Button>
+                            ))}
+                          </div>
+                        </Col>
+                        <Col sm="3">
+                          <span>Publication: {manga.publishYear}</span>
+                        </Col>
+                      </Row>
+                      <br></br>
+                      <Row>
+                        <Col>
+                          <Dropdown>
+                            <Dropdown.Toggle
+                              variant="outline"
+                              id="dropdown-basic"
+                            >
+                              <span>
+                                <i className="fa-regular fa-star"></i>
+                              </span>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                              <Dropdown.Item>(5) Masterpice</Dropdown.Item>
+                              <Dropdown.Item>(4) Good</Dropdown.Item>
+                              <Dropdown.Item>(3) Fine</Dropdown.Item>
+                              <Dropdown.Item>(2) Bad</Dropdown.Item>
+                              <Dropdown.Item>(1) Horrible</Dropdown.Item>
+                            </Dropdown.Menu>
+                          </Dropdown>
+                        </Col>
+                        <Col>
+                          &nbsp;&nbsp;
+                          <span>
+                            <button style={{ border: "none" }}>
+                              <i className="fa-regular fa-heart"></i>
+                            </button>
+                            &nbsp;100
+                          </span>
+                        </Col>
+                      </Row>
                     </>
                   ) : (
                     <p>Manga not found.</p>
                   )}
-                   <div>
-    </div>
+                  <div></div>
                 </div>
               )}
             </TrackVisibility>
@@ -134,21 +145,25 @@ function MangaDetail() {
             Chapters list
           </div>
           <Row style={{ padding: "20px" }}>
-            <Col>
-              <p>Chapter 1: You apply just the right amount of heat</p>
-              <p>
-                <i className="fa-regular fa-clock"></i>&nbsp;Group
-              </p>
-            </Col>
-            <Col xs lg="3">
-              <p>
-                <i className="fa-regular fa-clock"></i>&nbsp;2 days ago
-              </p>
-              <p>
-                <i className="fa-regular fa-clock"></i>
-                &nbsp;TestTestTestTestTestTestTes
-              </p>
-            </Col>
+            <p>Chapter 1: You apply just the right amount of heat</p>
+            <Row>
+              <Col className="col-sm-4 offset-sm-4">
+                <p className="text-end" >
+                  <i className="fa-regular fa-clock"></i>&nbsp;Group
+                </p>
+              </Col>
+              <Col >
+                <p className="text-end text-truncate" style={{maxWidth: "150px"}}>
+                  <i className="fa-regular fa-clock"></i>
+                  &nbsp;UploaderTestLong
+                </p>
+              </Col>
+              <Col>
+                <p className="text-end">
+                  <i className="fa-regular fa-clock"></i>&nbsp;2 days ago
+                </p>
+              </Col>
+            </Row>
             <p>Chapter 2: First breakfast together</p>
             <p>Chapter 3: A Toast with Beer and lamp chops</p>
           </Row>
@@ -156,9 +171,7 @@ function MangaDetail() {
         <div className="Manga-Container">
           <div className="Manga-Container-title">Comments</div>
           <Container>
-            <div className="commentSection" >
-              
-            </div>
+            <div className="commentSection"></div>
           </Container>
         </div>
       </Container>
