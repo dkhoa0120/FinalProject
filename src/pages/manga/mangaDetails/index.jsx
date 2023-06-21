47b11c82439417a4ea0e4192156bb5beb528bef4
huy26062002 <nonemu211@gmail.com>
2023-06-21 10:31:07 +0700

    Set chapters list

--- src/pages/manga/mangaDetails/index.jsx
+++ src/pages/manga/mangaDetails/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Dropdown, Button, Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { getMangaById } from "../../../service/Data.service";
 import TrackVisibility from "react-on-screen";
@@ -51,34 +51,101 @@ function MangaDetail() {
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={9}>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeIn" : ""
-                  }
-                >
-                  {manga ? (
-                    <>
-                      <h1 className="txt-rotate">{manga.originalTitle}</h1>
-                      <p>{manga.description}</p>
-                       <button>{manga.categories.map(c=>c.name)}</button> 
-                    </>
-                  ) : (
-                    <p>Manga not found.</p>
-                  )}
-                   <div>
-      {/* <h1>Manga Categories</h1>
-      <MangaCategoryList
-        categories={manga.categories.map(c=>c.name)}
-        
-      /> */}
-    </div>
-                </div>
-              )}
-            </TrackVisibility>
+            {manga ? (
+              <>
+                <Row>
+                  <h1 className="txt-rotate">{manga.originalTitle}</h1>
+                  <span>TestAuthorName</span>
+                  <p>{manga.description}</p>
+                </Row>
+                <Row>
+                  <Col>
+                    <button>{manga.categories.map((c) => c.name)}</button>
+                  </Col>
+                  <Col>
+                    <span>Publication: {manga.publishYear}</span>
+                  </Col>
+                </Row>
+                &nbsp;
+                <p>Alternative Titles: {manga.alternativeTitles}</p>
+                <Row>
+                  <Col>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="outline" id="dropdown-basic">
+                        <span>
+                          <i className="fa-regular fa-star"></i>
+                        </span>
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu>
+                        <Dropdown.Item>(5) Masterpice</Dropdown.Item>
+                        <Dropdown.Item>(4) Good</Dropdown.Item>
+                        <Dropdown.Item>(3) Fine</Dropdown.Item>
+                        <Dropdown.Item>(2) Bad</Dropdown.Item>
+                        <Dropdown.Item>(1) Horrible</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Col>
+                  <Col>
+                    &nbsp;&nbsp;
+                    <span>
+                      <i className="fa-regular fa-heart"></i>&nbsp;100
+                    </span>
+                  </Col>
+                </Row>
+              </>
+            ) : (
+              <p>Manga not found.</p>
+            )}
           </Col>
         </Row>
+      </Container>
+      <br></br>
+      <Container>
+        <div className="Manga-Container">
+          {/* <p
+            style={{
+              display: "flex",
+              justifyContent:"flex-end"
+            }}
+          >
+            <Button>Start Reading</Button>
+            &nbsp;
+            <Button>Latest Chapter</Button>
+          </p> */}
+          <div
+            className="Manga-Container-title"
+            style={{ textDecorationLine: "underline", marginBottom: "0" }}
+          >
+            Chapters list
+          </div>
+          <Row style={{ padding: "20px" }}>
+            <Col>
+              <p>Chapter 1: You apply just the right amount of heat</p>
+              <p>
+                <i className="fa-regular fa-clock"></i>&nbsp;Group
+              </p>
+            </Col>
+            <Col xs lg="3">
+              <p>
+                <i className="fa-regular fa-clock"></i>&nbsp;2 days ago
+              </p>
+              <p>
+                <i className="fa-regular fa-clock"></i>
+                &nbsp;TestTestTestTestTestTestTes
+              </p>
+            </Col>
+            <p>Chapter 2: First breakfast together</p>
+            <p>Chapter 3: A Toast with Beer and lamp chops</p>
+          </Row>
+        </div>
+        <div className="Manga-Container">
+          <div className="Manga-Container-title">Comments</div>
+          <Container>
+            <div className="commentSection" >
+              
+            </div>
+          </Container>
+        </div>
       </Container>
     </section>
   );
