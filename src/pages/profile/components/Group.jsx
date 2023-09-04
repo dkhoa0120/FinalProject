11e427072a09de1f8739cb9591a829f3bca22ce0
huy26062002 <nonemu211@gmail.com>
2023-09-04 08:48:33 +0700

    Add create manga list and add group

--- src/pages/profile/components/Group.jsx
+++ src/pages/profile/components/Group.jsx
@@ -1,84 +1,103 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
+import AsyncSelect from "react-select/async";
 
 export default function Groups() {
+  const [show, setShow] = useState(false);
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
-            <img
-              src="/img/error/coverNotFound.png"
-              style={{ width: "100px" }}
-              alt="group's cover"
-            ></img>
-            <div>
-              <p
-                className="text-limit-2"
-                style={{ fontWeight: "bold", marginBottom: "5px" }}
-              >
-                Group Name
-              </p>
-              <p className="text-limit-2" style={{ textAlign: "center" }}>
-                By ABC
-              </p>
-            </div>
-          </Col>
-          <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
-            <img
-              src="/img/error/coverNotFound.png"
-              style={{ width: "100px" }}
-              alt="group's cover"
-            ></img>
-            <div>
-              <p
-                className="text-limit-2"
-                style={{ fontWeight: "bold", marginBottom: "5px" }}
-              >
-                Group Name
-              </p>
-              <p className="text-limit-2" style={{ textAlign: "center" }}>
-                By ABC
-              </p>
-            </div>
-          </Col>
-          <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
-            <img
-              src="/img/error/coverNotFound.png"
-              style={{ width: "100px" }}
-              alt="group's cover"
-            ></img>
-            <div>
-              <p
-                className="text-limit-2"
-                style={{ fontWeight: "bold", marginBottom: "5px" }}
-              >
-                Group Name
-              </p>
-              <p className="text-limit-2" style={{ textAlign: "center" }}>
-                By ABC
-              </p>
-            </div>
-          </Col>
-          <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
-            <img
-              src="/img/error/coverNotFound.png"
-              style={{ width: "100px" }}
-              alt="group's cover"
-            ></img>
-            <div>
-              <p
-                className="text-limit-2"
-                style={{ fontWeight: "bold", marginBottom: "5px" }}
-              >
-                Group Name
-              </p>
-              <p className="text-limit-2" style={{ textAlign: "center" }}>
-                By ABC
-              </p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container fluid>
+      <Row>
+        <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
+          <img
+            src="/img/error/coverNotFound.png"
+            style={{ width: "100px", cursor: "pointer " }}
+            alt="group's cover"
+            onClick={() => setShow(true)}
+          ></img>
+          <div>
+            <p className="text-limit-2" style={{ fontWeight: "bold" }}>
+              Add more groups
+            </p>
+          </div>
+        </Col>
+        <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
+          <img
+            src="/img/error/coverNotFound.png"
+            style={{ width: "100px" }}
+            alt="group's cover"
+          ></img>
+          <div>
+            <p
+              className="text-limit-2"
+              style={{ fontWeight: "bold", marginBottom: "5px" }}
+            >
+              Group Name
+            </p>
+            <p className="text-limit-2" style={{ textAlign: "center" }}>
+              By ABC
+            </p>
+          </div>
+        </Col>
+        <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
+          <img
+            src="/img/error/coverNotFound.png"
+            style={{ width: "100px" }}
+            alt="group's cover"
+          ></img>
+          <div>
+            <p
+              className="text-limit-2"
+              style={{ fontWeight: "bold", marginBottom: "5px" }}
+            >
+              Group Name
+            </p>
+            <p className="text-limit-2" style={{ textAlign: "center" }}>
+              By ABC
+            </p>
+          </div>
+        </Col>
+        <Col className="d-flex align-items-center mx-4 mb-3 gap-2">
+          <img
+            src="/img/error/coverNotFound.png"
+            style={{ width: "100px" }}
+            alt="group's cover"
+          ></img>
+          <div>
+            <p
+              className="text-limit-2"
+              style={{ fontWeight: "bold", marginBottom: "5px" }}
+            >
+              Group Name
+            </p>
+            <p className="text-limit-2" style={{ textAlign: "center" }}>
+              By ABC
+            </p>
+          </div>
+        </Col>
+      </Row>
+      <Modal show={show} onHide={() => setShow(false)} size="xl">
+        <Modal.Header closeButton>
+          <Modal.Title>Add More Group</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Group</Form.Label>
+              <AsyncSelect />
+            </Form.Group>
+          </Form>
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <Button
+              variant="success"
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              Add
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </Container>
   );
 }
--- src/pages/upload/index.jsx
+++ src/pages/upload/index.jsx
@@ -6,9 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Upload() {
   const navigate = useNavigate();
-  const [oneShot, setOneShot] = useState(false);
   const [selectedImages, setSelectedImages] = useState([]);
-
   const fileInputRef = createRef();
 
   const handleImageChange = (e) => {
@@ -28,20 +26,16 @@ export default function Upload() {
     fileInputRef.current.click();
   };
 
-  const handleNavClick = () => {
-    navigate(`/`);
-  };
-
   return (
     <>
       <div style={{ fontSize: "25px", fontWeight: "bold" }}>
-        <button className="circle-button" onClick={handleNavClick}>
+        <button className="circle-button" onClick={() => navigate(-1)}>
           <i className="fa-solid fa-arrow-left"></i>
-        </button>{" "}
+        </button>
         Upload Chapter
       </div>
       &nbsp;
-      <Container>
+      <Container fluid>
         <Card className="uploader-card">
           <Row>
             <Col xs={4} md={2} xl={1}>
@@ -78,22 +72,11 @@ export default function Upload() {
         &nbsp;
         <hr />
         <Row>
-          <Col>
-            <Form.Check
-              type="checkbox"
-              onChange={() => setOneShot(!oneShot)}
-              size="lg"
-              label="This is a Oneshot"
-            ></Form.Check>
-          </Col>
-        </Row>
-        <Row>
           <Col md={4} lg={4} className="mb-1">
             <Form.Control
               type="search"
               placeholder="Number"
               aria-label="Number"
-              disabled={oneShot}
             />
           </Col>
           <Col md={4} lg={4} className="mb-1">
@@ -101,7 +84,6 @@ export default function Upload() {
               type="search"
               placeholder="Chapter number"
               aria-label="Chapter number"
-              disabled={oneShot}
             />
           </Col>
           <Col md={4} lg={4} className="mb-1">
@@ -110,68 +92,64 @@ export default function Upload() {
         </Row>
         &nbsp;
         <hr />
-        <Row>
-          <Form.Label>Pages</Form.Label>
-          <div className="image-container justify-left flex-wrap mb-4">
-            {selectedImages.map((image, index) => (
-              <div key={index} className="pages-upload-card flex-grow-0">
-                <img
-                  className="image"
-                  src={URL.createObjectURL(image)}
-                  alt="pages"
-                />
-                <button
-                  className="delete-button"
-                  onClick={() => handleImageDelete(index)}
-                >
-                  <i className="fa-solid fa-xmark"></i>
-                </button>
-                <button className="drag-button">
-                  <i className="fa-solid fa-arrows-up-down-left-right"></i>
-                </button>
-                <div className="image-label">{image.name}</div>
-              </div>
-            ))}
-            <div className="label" onClick={handleFileInputClick}>
-              <i className="fa-solid fa-plus" />
-              <input
-                className="input-pages"
-                type="file"
-                accept="image/*"
-                ref={fileInputRef}
-                onChange={handleImageChange}
-                multiple
+        <div>Pages</div>
+        <div className="image-container justify-left flex-wrap mb-4">
+          {selectedImages.map((image, index) => (
+            <div key={index} className="pages-upload-card flex-grow-0">
+              <img
+                className="image"
+                src={URL.createObjectURL(image)}
+                alt="pages"
               />
-            </div>
-          </div>
-          {selectedImages.length > 0 ? (
-            <div>
               <button
-                className="btn btn-dark"
-                onClick={() => setSelectedImages([])}
+                className="delete-button"
+                onClick={() => handleImageDelete(index)}
               >
-                Remove all pages
+                <i className="fa-solid fa-xmark"></i>
               </button>
+              <button className="drag-button">
+                <i className="fa-solid fa-arrows-up-down-left-right"></i>
+              </button>
+              <div className="image-label">{image.name}</div>
             </div>
-          ) : (
-            <></>
-          )}
-        </Row>
-        <Row>
-          <Col className="d-flex justify-content-end">
+          ))}
+          <div className="label" onClick={handleFileInputClick}>
+            <i className="fa-solid fa-plus" />
+            <input
+              className="input-pages"
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleImageChange}
+              multiple
+            />
+          </div>
+        </div>
+        {selectedImages.length > 0 ? (
+          <div>
             <button
-              className="new-to-you"
-              style={{
-                color: "white",
-                fontSize: "20px",
-                fontWeight: "bold",
-                padding: "6px 15px",
-              }}
+              className="btn btn-dark"
+              onClick={() => setSelectedImages([])}
             >
-              Upload
+              Remove all pages
             </button>
-          </Col>
-        </Row>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="d-flex justify-content-end">
+          <button
+            className="new-to-you"
+            style={{
+              color: "white",
+              fontSize: "20px",
+              fontWeight: "bold",
+              padding: "6px 15px",
+            }}
+          >
+            Upload
+          </button>
+        </div>
       </Container>
     </>
   );
