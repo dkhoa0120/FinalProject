17766e632ff4bd25c687ea18246a1af221e5467f
DKHOA111 <dkhoa0120@gmail.com>
2023-10-24 11:25:24 +0700

    add see more notification

--- src/layout/header/components/notificationSection.jsx
+++ src/layout/header/components/notificationSection.jsx
@@ -5,13 +5,16 @@ import { useContext } from "react";
 import * as notificationApi from "../../../service/api.notification";
 import { NotificationContext } from "../../../context/NotificationContext";
 import { useEffect } from "react";
+import { useRef } from "react";
 
 export default function NotificationSection() {
   const sortOptions = ["Chapter", "Request", "Follower", "Group"];
   const [sortOption, setSortOption] = useState(sortOptions[0]);
   const [showNoti, setShowNoti] = useState(false);
   const { notificationCounts } = useContext(NotificationContext);
-
+  const [loadingNoti, setLoadingNoti] = useState(false);
+  const [outOfNoti, setOutOfNoti] = useState(false);
+  const notiHeader = useRef(null);
   const [notifications, setNotifications] = useState([]);
 
   const CustomNotiFication = () => (
@@ -21,9 +24,11 @@ export default function NotificationSection() {
         className="fa-solid fa-bell"
         style={{ fontSize: "25px", cursor: "pointer" }}
       ></i>
-      <Badge className="notification-number" bg="danger">
-        {Object.values(notificationCounts).reduce((a, b) => a + b)}
-      </Badge>
+      {Object.values(notificationCounts).reduce((a, b) => a + b) > 0 && (
+        <Badge className="notification-number" bg="danger">
+          {Object.values(notificationCounts).reduce((a, b) => a + b)}
+        </Badge>
+      )}
     </>
   );
 
@@ -40,6 +45,61 @@ export default function NotificationSection() {
     fetchNotifications({ type: sortOption });
   }, [sortOption, showNoti]);
 
+  const handleSeeMoreNoti = async (type, createdAtCursor) => {
+    try {
+      const newNotifications = await notificationApi.getNotifications({
+        type: type,
+        createdAtCursor: createdAtCursor,
+      });
+      setNotifications([...notifications, ...newNotifications.data]);
+
+      // Set outOfComment to disable loading more comment in scroll event below
+      if (newNotifications.data.length > 0) {
+        setOutOfNoti(false);
+      } else {
+        setOutOfNoti(true);
+      }
+    } catch (error) {
+      console.error("Error fetching more members:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!notiHeader.current) return;
+
+    const notificationDiv = notiHeader.current.parentNode;
+
+    console.log("sortOption", sortOption);
+    console.log(
+      "notifications",
+      notifications[notifications.length - 1]?.createdAt
+    );
+    const handleScroll = () => {
+      const { scrollHeight, scrollTop, clientHeight } = notificationDiv;
+
+      // Check if you've scrolled to the bottom
+      if (
+        scrollHeight - scrollTop - clientHeight <= 5 &&
+        notifications.length > 0 &&
+        !outOfNoti
+      ) {
+        setLoadingNoti(true);
+        setTimeout(() => {
+          handleSeeMoreNoti(
+            sortOption,
+            notifications[notifications.length - 1]?.createdAt
+          );
+          setLoadingNoti(false);
+        }, 1000);
+      }
+    };
+    notificationDiv.addEventListener("scroll", handleScroll);
+
+    return () => {
+      notificationDiv.removeEventListener("scroll", handleScroll);
+    };
+  }, [sortOption, notifications, showNoti, outOfNoti, handleSeeMoreNoti]);
+
   return (
     <>
       <Button as={CustomNotiFication} />
@@ -49,56 +109,65 @@ export default function NotificationSection() {
         placement="end"
         id={`offcanvasNavbar-expand-false`}
         aria-labelledby={`offcanvasNavbarLabel-expand-false`}
-        style={{ width: "470px", maxHeight: "80vh" }}
+        style={{ width: "470px", maxHeight: "50vh" }}
       >
         <Offcanvas.Header closeButton style={{ paddingBottom: "0px" }}>
           <h3 style={{ fontWeight: "bold" }}>Notifications</h3>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <div className="notification-button">
-            {sortOptions.map((option, index) => (
-              <Button
-                key={index}
-                variant={sortOption === option ? "dark" : "light"}
-                onClick={() => setSortOption(option)}
-                style={{ marginBottom: "10px" }}
-              >
-                {option + " "}
-                <Badge pill bg="danger">
-                  {notificationCounts[option.toLowerCase()]}{" "}
-                </Badge>
-              </Button>
-            ))}
+          <div ref={notiHeader}>
+            <div className="notification-button">
+              {sortOptions.map((option, index) => (
+                <Button
+                  key={index}
+                  variant={sortOption === option ? "dark" : "light"}
+                  onClick={() => setSortOption(option)}
+                  style={{ marginBottom: "10px" }}
+                >
+                  {option + " "}
+                  {notificationCounts[option.toLowerCase()] > 0 && (
+                    <Badge pill bg="danger">
+                      {notificationCounts[option.toLowerCase()]}{" "}
+                    </Badge>
+                  )}
+                </Button>
+              ))}
+            </div>
+            {sortOption === "Chapter" && (
+              <Notification
+                notifications={notifications}
+                type={"Chapter"}
+                close={() => setShowNoti(false)}
+              />
+            )}
+            {sortOption === "Request" && (
+              <Notification
+                notifications={notifications}
+                type={"Request"}
+                close={() => setShowNoti(false)}
+              />
+            )}
+            {sortOption === "Follower" && (
+              <Notification
+                notifications={notifications}
+                type={"Follower"}
+                close={() => setShowNoti(false)}
+              />
+            )}
+            {sortOption === "Group" && (
+              <Notification
+                notifications={notifications}
+                type={"Group"}
+                close={() => setShowNoti(false)}
+              />
+            )}
           </div>
-          {sortOption === "Chapter" && (
-            <Notification
-              notifications={notifications}
-              type={"Chapter"}
-              close={() => setShowNoti(false)}
-            />
-          )}
-          {sortOption === "Request" && (
-            <Notification
-              notifications={notifications}
-              type={"Request"}
-              close={() => setShowNoti(false)}
-            />
-          )}
-          {sortOption === "Follower" && (
-            <Notification
-              notifications={notifications}
-              type={"Follower"}
-              close={() => setShowNoti(false)}
-            />
-          )}
-          {sortOption === "Group" && (
-            <Notification
-              notifications={notifications}
-              type={"Group"}
-              close={() => setShowNoti(false)}
-            />
-          )}
         </Offcanvas.Body>
+        {loadingNoti && (
+          <div className="d-flex justify-content-center">
+            <div className="spinner-border" role="status"></div>
+          </div>
+        )}
       </Offcanvas>
     </>
   );
